e5092a8ff454d580a255157860a86e2b223d5f2d
lentqq <lentqq@gmail.com>
2019-03-11 11:11:58 +0200

    Add new file

---
+++ 02.city.js
@@ -0,0 +1,12 @@
+function city(name, area, population, country, postCode) {
+    let city = {
+        name, area, population, country, postCode
+    }
+
+    
+    for (const pair in city) {
+      console.log(`${pair} -> ${city[pair]}`);
+    }
+}
+
+city('Michuryn', 25, 6000, 'Bulgaria', 8620)
