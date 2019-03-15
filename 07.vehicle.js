f9146f7d45192139eeedce57aa4034ff395b74e6
lentqq <lentqq@gmail.com>
2019-03-15 07:45:16 +0200

    Finished the problem

--- 07.vehicle.js
+++ 07.vehicle.js
@@ -1,1 +1,21 @@
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+         this.type = type;
+         this.model = model;
+         this.parts = parts;
+         this.parts.engine;
+         this.parts.power;
+         this.parts.quality = parts.engine * parts.power;
+         this.fuel = fuel;
+    }
 
+     drive(usedFuel) {
+         this.fuel -= usedFuel;
+    }
+}
+
+let parts = {engine: 7, power: 200};
+let vehicle = new Vehicle('Porshe', 'Cayenne', parts, 500);
+vehicle.drive(200);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
